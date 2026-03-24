cb31ab2b12fedb3a9a23dd0e68e57c29e88ec3f6
Edward Kang <mail@edwardkang.io>
2026-03-24 20:32:30 +0100

    feat: add admin auth

--- packages/framework-adapters/express/src/router.ts
+++ packages/framework-adapters/express/src/router.ts
@@ -1,13 +1,26 @@
 import type { LibreFlag } from "libreflag";
 import { Router } from "express";
+import type { RequestHandler } from "express";
 import { OFREPRouter } from "./routes/ofrep.js";
 import { FlagsRouter } from "./routes/flags.js";
+import { ROUTES } from "./constants/routes.js";
 
-export function LibreFlagExpress(provider: LibreFlag): Router {
+export interface LibreFlagExpressOptions {
+  adminAuthMiddleware?: RequestHandler;
+}
+
+export function LibreFlagExpress(
+  provider: LibreFlag,
+  {
+    adminAuthMiddleware = (_req, res) => {
+      res.status(403).send();
+    },
+  }: LibreFlagExpressOptions = {},
+): Router {
   const router = Router();
 
-  router.use(OFREPRouter(provider));
-  router.use(FlagsRouter(provider));
+  router.use(ROUTES.OFREP, OFREPRouter(provider));
+  router.use(ROUTES.FLAGS, adminAuthMiddleware, FlagsRouter(provider));
 
   return router;
 }
