7d83a247b192f5ff9ab8eba44c7b3c3edca9744c
Edward Kang <mail@edwardkang.io>
2026-03-28 12:35:57 +0100

    chore(postgres): rename schema

--- packages/db-adapters/postgres/src/db/constants.ts
+++ packages/db-adapters/postgres/src/db/constants.ts
@@ -4,6 +4,6 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const MIGRATIONS_DIR = path.join(__dirname, "..", "..", "drizzle");
-export const MIGRATIONS_SCHEMA = "feature_flags";
-export const MIGRATIONS_TABLE = "__feature_flags_migrations";
+export const MIGRATIONS_SCHEMA = "libreflag";
+export const MIGRATIONS_TABLE = "__libreflag_migrations";
 export const ADVISORY_LOCK_ID = 9182;
